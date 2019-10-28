d2175200f3645ae5438a4294b40a2fc78fe3da0e
Renan Diego <renan.ccomp@gmail.com>
2019-10-28 10:02:12 -0300

    change url server

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
 export let url = axios.create({
-  // baseURL: "http://177.23.191.191:3000/",
-  baseURL: "http://localhost:3000/"
+  baseURL: "http://177.23.191.191:3000/",
+  // baseURL: "http://localhost:3000/"
 })
 
 export let urlHeaders = axios.create({
-  // baseURL: "http://177.23.191.191:3000/",
-  baseURL: "http://localhost:3000/",
+  baseURL: "http://177.23.191.191:3000/",
+  // baseURL: "http://localhost:3000/",
   headers: {
     'Accept': 'application/json',
     'access-token': localStorage.getItem('access-token'),
